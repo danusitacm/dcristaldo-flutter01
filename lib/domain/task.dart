4aae7f6c09619d437688f5e324f733ef2e78c443
danusitacm <danielacristaldomo@gmail.com>
2025-04-08 09:25:23 -0300

    Añade el campo pasos a tadk.dart

--- lib/domain/task.dart
+++ lib/domain/task.dart
@@ -4,18 +4,19 @@ class Task {
   final String detail;
   final DateTime date;
   final DateTime fechaLimite; // Campo para la fecha límite
-
+  final List<String> pasos;
   Task({
     required this.title,
     this.type = 'Normal',
     required this.detail,
     DateTime? date,
     DateTime? fechaLimite,
-    int diasParaLimite = 7, // Número de días para calcular la fecha límite
+    int diasParaLimite = 7,
+    this.pasos = const ["vacio"],
   }) : date = date ?? DateTime.now(),
        fechaLimite = (date ?? DateTime.now()).add(
          Duration(days: diasParaLimite),
-       ); // Calcula la fecha límite
+       ); // Inicializa el campo pasos
 
   bool isUrgent() {
     return type == 'Urgente';
