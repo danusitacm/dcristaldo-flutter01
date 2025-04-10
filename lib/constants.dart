e7a11aabba0f28d401c3d8d92f7f2e22dddc3853
danusitacm <danielacristaldomo@gmail.com>
2025-04-10 10:38:15 -0300

    Modifica la constante PASOS_VACIO

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,6 +3,6 @@ const String EMPTY_LIST = 'No hay tareas';
 const String TASK_TYPE_LABEL = 'Tipo: ';
 const String PASOS_TITULO = 'Pasos para completar';
 const String FECHA_LIMITE = 'Fecha límite: ';
-const String PASOS_VACIO = 'No hay pasos disponibles,';
+const String PASOS_VACIO = 'No hay pasos disponibles.';
 const String TAREA_ELIMINADA = 'Tarea eliminada';
 const String TIPO_TAREA = 'Tipo: ';
