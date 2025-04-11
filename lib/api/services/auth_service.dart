965f790b429a310c28d351b71502b8c77c2053c8
danusitacm <danielacristaldomo@gmail.com>
2025-04-11 08:23:01 -0300

    Comenta los print en auth_service

--- lib/api/services/auth_service.dart
+++ lib/api/services/auth_service.dart
@@ -4,7 +4,7 @@ class AuthService {
   Future<bool> login(String username, String password) async {
     // Verifica que las credenciales no sean nulas ni vacías
     if (username.isEmpty || password.isEmpty) {
-      print('Error: Usuario o contraseña no pueden estar vacíos.');
+      //print('Error: Usuario o contraseña no pueden estar vacíos.');
       return false;
     }
 
@@ -12,9 +12,9 @@ class AuthService {
     await Future.delayed(const Duration(seconds: 1));
 
     // Imprime las credenciales en la consola
-    print('Intentando iniciar sesión con:');
-    print('Usuario: $username');
-    print('Contraseña: $password');
+    //print('Intentando iniciar sesión con:');
+    //print('Usuario: $username');
+    //print('Contraseña: $password');
 
     // Retorna true para simular un inicio de sesión exitoso
     return true;
