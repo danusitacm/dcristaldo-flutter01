43041639bf2d43cd74d33c3d0e9ac4afaf848f75
danusitacm <danielacristaldomo@gmail.com>
2025-04-11 10:12:41 -0300

    Modifica los datos en QuestionRepository

--- lib/data/question_repository.dart
+++ lib/data/question_repository.dart
@@ -3,19 +3,19 @@ import 'package:dcristaldo/domain/question.dart';
 class QuestionRepository {
   final List<Question> _questions = [
     Question(
-      question: '¿Cuál es la capital de Francia?',
-      options: ['París', 'Londres', 'Madrid', 'Berlín'],
-      correctAnswer: 'París',
+      questionText: '¿Cuál es la capital de Francia?',
+      answerOptions: ['París', 'Londres', 'Madrid', 'Berlín'],
+      correctAnswerIndex: 0, // Índice de la respuesta correcta
     ),
     Question(
-      question: '¿Cuál es el planeta más grande del sistema solar?',
-      options: ['Tierra', 'Marte', 'Júpiter', 'Saturno'],
-      correctAnswer: 'Júpiter',
+      questionText: '¿Cuál es el planeta más grande del sistema solar?',
+      answerOptions: ['Tierra', 'Marte', 'Júpiter', 'Saturno'],
+      correctAnswerIndex: 2, // Índice de la respuesta correcta
     ),
     Question(
-      question: '¿En qué año llegó el hombre a la luna?',
-      options: ['1965', '1969', '1972', '1980'],
-      correctAnswer: '1969',
+      questionText: '¿En qué año llegó el hombre a la luna?',
+      answerOptions: ['1965', '1969', '1972', '1980'],
+      correctAnswerIndex: 1, // Índice de la respuesta correcta
     ),
   ];
 
