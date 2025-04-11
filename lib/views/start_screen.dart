2cc3b66510a78e9274dc0b7bb1ab69de10e3eb2f
danusitacm <danielacristaldomo@gmail.com>
2025-04-11 10:20:15 -0300

    Refactoriza StartScreen para usar BaseScreen

--- lib/views/start_screen.dart
+++ lib/views/start_screen.dart
@@ -1,3 +1,5 @@
+import 'package:dcristaldo/constants.dart';
+import 'package:dcristaldo/views/base_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:dcristaldo/views/game_screen.dart';
 
@@ -6,17 +8,28 @@ class StartScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Juego de Preguntas')),
+    return BaseScreen(
+      appBar: AppBar(title: const Text('Inicio')),
       body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const GameScreen()),
-            );
-          },
-          child: const Text('Comenzar Juego'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              welcomeMessage,
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const GameScreen()),
+                );
+              },
+              child: const Text('Comenzar Juego'),
+            ),
+          ],
         ),
       ),
     );
