70bb598efa1c717b23305fe6112d776590934ee2
danusitacm <danielacristaldomo@gmail.com>
2025-04-13 20:04:04 -0300

    Agrega el texto questionCounterText

--- lib/views/game_screen.dart
+++ lib/views/game_screen.dart
@@ -17,7 +17,8 @@ class GameScreenState extends State<GameScreen> {
   int currentQuestionIndex = 0; // Índice de la pregunta actual
   int userScore = 0; // Puntuación del usuario
   bool? isCorrectAnswer; // Indica si la respuesta seleccionada es correcta
-
+  int questionCounterText = 1;
+  
   @override
   void initState() {
     super.initState();
@@ -39,6 +40,7 @@ class GameScreenState extends State<GameScreen> {
       // Avanza a la siguiente pregunta o navega a la pantalla de resultados
       if (currentQuestionIndex < questionList.length - 1) {
         currentQuestionIndex++;
+        questionCounterText=currentQuestionIndex + 1;
         isCorrectAnswer = null; // Reinicia el estado de la respuesta
       } else {
         Navigator.pushReplacement(
@@ -56,6 +58,7 @@ class GameScreenState extends State<GameScreen> {
   @override
   Widget build(BuildContext context) {
     final question = questionList[currentQuestionIndex];
+    int totalQuestion = questionList.length;
 
     return Scaffold(
       appBar: AppBar(title: const Text(gameTitle)),
@@ -64,6 +67,7 @@ class GameScreenState extends State<GameScreen> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            Text('Pregunta $questionCounterText de  $totalQuestion'),
             Text(
               question.questionText,
               style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
