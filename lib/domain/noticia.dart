cd4bc5807fe5852449d1cf7fc59275ecd9afd3d8
danusitacm <danielacristaldomo@gmail.com>
2025-04-16 08:28:57 -0300

    Inicializa estructura para modulo noticias

---
+++ lib/domain/noticia.dart
@@ -0,0 +1,12 @@
+class Noticia {
+  final String titulo;
+  final String descripcion;
+  final String fuente;
+  final DateTime publicadaEl;
+  Noticia({
+    required this.titulo,
+    required this.descripcion,
+    required this.fuente,
+    required this.publicadaEl,
+  });
+}
