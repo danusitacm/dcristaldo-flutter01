edfe0762c3d502a5f61ff5cc9e9ab7ab9994da2b
danusitacm <danielacristaldomo@gmail.com>
2025-04-16 13:07:18 -0300

    Modifica basandose en el mockup de la tarea

--- lib/components/noticia_card.dart
+++ lib/components/noticia_card.dart
@@ -13,72 +13,138 @@ class NoticiaCard extends StatelessWidget {
   Widget build(BuildContext context) {
     final DateFormat dateFormat = DateFormat(Constants.formatoFecha);
 
-    return Card(
-      margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-      elevation: 4.0,
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            // Contenido de la noticia
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  // Título en negrita con fontSize 18
-                  Text(
-                    noticia.titulo,
-                    style: const TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  const SizedBox(height: 8.0),
-                  // Descripción con un máximo de 3 líneas
-                  Text(
-                    noticia.descripcion,
-                    maxLines: 3,
-                    overflow: TextOverflow.ellipsis,
-                    style: const TextStyle(fontSize: 14),
-                  ),
-                  const SizedBox(height: 8.0),
-                  // Fuente en cursiva
-                  Text(
-                    noticia.fuente,
-                    style: const TextStyle(
-                      fontStyle: FontStyle.italic,
-                      fontSize: 12,
-                    ),
-                  ),
-                  const SizedBox(height: 4.0),
-                  // Fecha de publicación en formato dd/MM/yyyy HH:mm
-                  Text(
-                    dateFormat.format(noticia.publicadaEl),
-                    style: const TextStyle(fontSize: 12, color: Colors.grey),
-                  ),
-                ],
-              ),
-            ),
-            const SizedBox(width: 16.0),
-            // Imagen aleatoria de Picsum
-            Column(
+    return Column(
+      children: [
+        Card(
+          margin: EdgeInsets.zero, // Sin margen entre los elementos
+          elevation: 0.0, // Sin sombra
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.zero, // Sin bordes redondeados
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(
+              10.0,
+            ), // Padding general para el contenido
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(8.0),
-                  child: Image.network(
-                    'https://picsum.photos/100', // Imagen aleatoria de 100x100
-                    width: 100,
-                    height: 100,
-                    fit: BoxFit.cover,
+                // Contenido de la noticia con padding adicional a la izquierda
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                      left: 10.0,
+                      top: 10.0, // Padding arriba del texto
+                      bottom: 10.0, // Padding abajo del texto
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        // Título en negrita con fontSize 18
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 8.0),
+                          child: Text(
+                            noticia.titulo,
+                            maxLines: 2, // Limitar a 2 líneas
+                            overflow:
+                                TextOverflow
+                                    .ellipsis, // Mostrar elipsis si el texto es muy largo
+                            style: const TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                        // Fuente en cursiva
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 4.0),
+                          child: Text(
+                            noticia.fuente,
+                            style: const TextStyle(
+                              fontStyle: FontStyle.italic,
+                              fontSize: 12,
+                            ),
+                          ),
+                        ),
+                        // Descripción con un máximo de 3 líneas
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 8.0),
+                          child: Text(
+                            noticia.descripcion,
+                            maxLines: 3,
+                            overflow: TextOverflow.ellipsis,
+                            style: const TextStyle(fontSize: 14),
+                          ),
+                        ),
+                        // Fecha de publicación en formato dd/MM/yyyy HH:mm
+                        Text(
+                          dateFormat.format(noticia.publicadaEl),
+                          style: const TextStyle(
+                            fontSize: 12,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-                CommonWidgetsHelper.buildSpacing(Constants.espacioAlto),
+                // Imagen aleatoria de Picsum con padding adicional a la derecha
+                Padding(
+                  padding: const EdgeInsets.only(
+                    right: 10.0,
+                    top: 10.0, // Padding arriba de la imagen
+                    bottom: 10.0, // Padding abajo de la imagen
+                  ),
+                  child: Column(
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(
+                          16.0,
+                        ), // Borde redondeado
+                        child: Image.network(
+                          'https://picsum.photos/150/100', // Imagen rectangular de 150x100
+                          width: 120, // Ancho ajustado
+                          height: 80, // Altura ajustada
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      CommonWidgetsHelper.buildSpacing(Constants.espacioAlto),
+                      // Row con botones debajo de la imagen
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          IconButton(
+                            icon: const Icon(Icons.star),
+                            onPressed: () {
+                              // Acción para marcar como favorito
+                            },
+                          ),
+                          IconButton(
+                            icon: const Icon(Icons.share),
+                            onPressed: () {
+                              // Acción para compartir
+                            },
+                          ),
+                          IconButton(
+                            icon: const Icon(Icons.more_vert),
+                            onPressed: () {
+                              // Acción para mostrar más opciones
+                            },
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
               ],
             ),
-          ],
+          ),
         ),
-      ),
+        const Divider(
+          color: Colors.grey, // Color de la línea
+          thickness: 1.0, // Grosor de la línea
+          height: 1.0, // Altura del espacio ocupado por el Divider
+        ),
+      ],
     );
   }
 }
