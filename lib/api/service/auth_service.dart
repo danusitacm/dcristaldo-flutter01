4d34405b575999f11e2c359da5a9d5b3e9bcd849
danusitacm <danielacristaldomo@gmail.com>
2025-06-08 00:04:29 -0300

    Limita los usuarios

--- lib/api/service/auth_service.dart
+++ lib/api/service/auth_service.dart
@@ -5,6 +5,40 @@ import 'package:dcristaldo/exceptions/api_exception.dart';
 
 class AuthService extends BaseService {
   AuthService() : super();
+
+  Future<LoginResponse> loginLimitado(LoginRequest request) async {
+    try {
+      dynamic data;
+      final List<LoginRequest> usuariosTest = [
+        const LoginRequest(username: 'profeltes', password: 'sodep'),
+        const LoginRequest(username: 'Moni', password: 'sodep'),
+        const LoginRequest(username: 'sodep', password: 'sodep'),
+        const LoginRequest(username: 'visitante', password: 'sodep'),
+      ];
+
+      bool credencialesValidas = usuariosTest.any(
+        (usuario) =>
+            usuario.username == request.username &&
+            usuario.password == request.password,
+      );
+       if (credencialesValidas) {
+        data = await postUnauthorized('/login', data: request.toJson());
+      }
+
+      if (data != null) {
+        return LoginResponseMapper.fromMap(data);
+      } else {
+        throw ApiException('Error de autenticación: respuesta vacía');
+      }
+    } catch (e) {
+      if (e is ApiException) {
+        rethrow;
+      } else {
+        throw ApiException('Error en login');
+      }
+    }
+  }
+  
   Future<LoginResponse> login(LoginRequest request) async {
     try {
       final data = await postUnauthorized(
